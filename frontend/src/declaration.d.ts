d4d8c473c4a0db3cb6ccd30d3b12804e76484ca4
Thomas Gauthier-Caron <thomas.gauthier.c@gmail.com>
2023-01-18 03:47:52 -0500

    clean

--- frontend/src/declaration.d.ts
+++
@@ -1,9 +0,0 @@
-export { };
-
-declare module "solid-js" {
-    namespace JSX {
-        interface IntrinsicElements {
-            "x-entity": JSX.IntrinsicElements["div"] & { [key: string]: any };
-        }
-    }
-}
--- packages/interpreter/src/node.d.ts
+++
@@ -1,9 +0,0 @@
-import { JSXElement } from "./react2"
-
-export type FunctionComponent<T> = (props: T & { children?: any[] }) => JSXElement
-
-export type EntityNodeEvaluatorProps<T> = T & {setEvaluate: (evaluate: () => (eid: number) => any) => void }
-export type EntityNodeEvaluator<T> = FunctionComponent<EntityNodeEvaluatorProps<T>>
-
-export type NodeEvaluatorProps<T> = T & {setEvaluate: (evaluate: () => () => void) => void }
-export type NodeEvaluator<T> = FunctionComponent<NodeEvaluatorProps<T>>
